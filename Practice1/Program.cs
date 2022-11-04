3f1d440ccff9a75a8acab72fde0cda89ecddded0
Oleg <o.antonenko2011@yandex.ru>
2022-11-04 13:08:31 +0300

    Lection4

---
+++ Practice1/Program.cs
@@ -0,0 +1,41 @@
+﻿string [,] table = new string [2, 5];
+table [1, 2] = "Слово";
+// String.Empty
+// table [0,0]  table [0,1]
+// table [1,0]  table [1,1]
+
+// for (int rows = 0; rows < 2; rows ++)
+// {
+//     for (int columns = 0; columns < 5; columns ++)
+//     {
+//         Console.WriteLine ($"{table[rows, columns]} --");
+//     }
+// }
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength (0); i ++)
+    {
+        for (int j = 0; j < matr.GetLength (1); j ++)
+        {
+            Console.Write ($"{matr[i, j]}  ");
+        }
+    Console.WriteLine (); 
+    }
+}
+
+void FillArray (int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength (0); i ++)
+    {
+        for (int j = 0; j < matr.GetLength (1); j ++)
+        {
+            matr [i, j] = new Random().Next (1, 10);
+        }
+    }
+}
+
+int  [,] matrix = new int [3,4];
+FillArray (matrix);
+PrintArray (matrix);
